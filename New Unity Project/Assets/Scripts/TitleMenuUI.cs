f87f39225e311cacfbf4e651c051812de6db4729
bigswimmer2 <brettpinfold2@gmail.com>
2021-10-20 14:00:51 -0500

    debugging for future

--- New Unity Project/Assets/Scripts/TitleMenuUI.cs
+++ New Unity Project/Assets/Scripts/TitleMenuUI.cs
@@ -8,11 +8,13 @@ public class TitleMenuUI : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame()
     {
+        Debug.Log("Start Game");
         SceneManager.LoadScene("Main");
     }
 
     public void QuitGame()
     {
+        Debug.Log("Quit Game");
         if (UnityEditor.EditorApplication.isPlaying)
         {
             UnityEditor.EditorApplication.isPlaying = false;
